86d12333d5a940b66a2320c8787be070918434f1
osamamaktari <osamaaltal01@gmail.com>
2025-09-21 21:26:23 +0300

    feat:install some packes and libraries

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -5,16 +5,37 @@
 
 // createApp(App).mount('#app')
 
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
 
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '../src/router'; 
 
 import './assets/main.css'
+const options = {
+  position: "top-right",
+  timeout: 5000, // 5 ثواني
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: "button",
+  icon: true,
+  rtl: false,
+  transition: "Vue-Toastification__bounce",
+  maxToasts: 10,
+  newestOnTop: true
+};
+
 
 
 const app = createApp(App);
-
+app.use(Toast, options);
 app.use(router); 
 app.mount('#app');
 
